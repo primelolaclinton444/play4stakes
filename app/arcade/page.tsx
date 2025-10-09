645d4d98ae665a63ab40354bcba95d0973c39116
primelolaclinton444 <lolaclinton444@gmail.com>
2025-10-09 09:26:28 -0500

    Create page.tsx

---
+++ app/arcade/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import Play4StakesWalletMVP from '@/components/Play4StakesWalletMVP';
+
+export default function ArcadePage() {
+  return <Play4StakesWalletMVP />;
+}
